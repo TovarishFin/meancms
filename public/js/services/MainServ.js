2a2115fad92e505b17f9f1fefc4c6d73901d84cd
TovarishFin <tovarishfin@yandex.ru>
2016-04-20 12:21:39 +0200

    fixed usertype auth still need to add different routes for each usertype

--- public/js/services/MainServ.js
+++ public/js/services/MainServ.js
@@ -10,118 +10,181 @@ angular.module('MainAppServices',[])
 		};
 			//api calls for products
 			call.getProducts = function(){
-				return $http.get('/reqauth/api/products').success(function(data){
-				angular.copy(data, call.products);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.get('/reqauth/'+data.usertype+'/api/products').success(function(data){
+						angular.copy(data, call.products);
+					});
 				});
 			};
 			call.createProduct = function(product){
-				return $http.post('/reqauth/api/products', product).success(function(data){
-					call.products.push(data);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/products', product).success(function(data){
+							call.products.push(data);
+					});
 				});
 			};
 			call.updateProduct = function(product){
-				return $http.post('/reqauth/api/products/update', product).success(function(data){
-					call.getProducts();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/products/update', product).success(function(data){
+							call.getProducts();
+					});
 				});
 			};
 			call.deleteProduct = function(id){
-				return $http.post('/reqauth/api/products/delete',{'_id': id}).success(function(data){
-					call.getProducts();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/products/delete',{'_id': id}).success(function(data){
+							call.getProducts();
+					});
 				});
 			};
 			//api calls for payment methods
 			call.getPmethods = function(){
-				return $http.get('/reqauth/api/pmethods').success(function(data){
-					angular.copy(data, call.pMethods);
+				call.getUserInfo()
+					.success(function(user){
+						return $http.get('/reqauth/'+user.usertype+'/api/pmethods').success(function(data){
+							angular.copy(data, call.pMethods);
+					});
 				});
 			};
 			call.createPmethod = function(pMethod){
-				return $http.post('/reqauth/api/pmethods', pMethod).success(function(data){
-					call.pMethods.push(data);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/pmethods', pMethod).success(function(data){
+							call.pMethods.push(data);
+					});
 				});
 			};
 			call.updatePmethod = function(pMethod){
-				return $http.post('/reqauth/api/pmethods/update', pMethod).success(function(data){
-					call.getPmethods();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/pmethods/update', pMethod).success(function(data){
+							call.getPmethods();
+					});
 				});
 			};
 			call.deletePmethod = function(id){
-				return $http.post('/reqauth/api/pmethods/delete', {'_id':id}).success(function(data){
-					call.getPmethods();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/pmethods/delete', {'_id':id}).success(function(data){
+							call.getPmethods();
+					});
 				});
 			};
 			//api calls for customers
 			call.getCustomers = function(){
-				return $http.get('/reqauth/api/customers').success(function(data){
-					angular.copy(data, call.customers);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.get('/reqauth/'+data.usertype+'/api/customers').success(function(data){
+							angular.copy(data, call.customers);
+					});
 				});
 			};
 			//removed promise... need to update others...
 			call.createCustomer = function(customer){
-				return $http.post('/reqauth/api/customers', customer)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/customers', customer)
+					});
 			};
 			call.updateCustomer = function(customer){
-				return $http.post('/reqauth/api/customers/update', customer).success(function(data){
-					call.getCustomers();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/customers/update', customer).success(function(data){
+							call.getCustomers();
+					});
 				});
 			};
 			call.deleteCustomer = function(id){
-				return $http.post('/reqauth/api/customers/delete',{'_id': id}).success(function(data){
-					call.getCustomers();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/customers/delete',{'_id': id}).success(function(data){
+							call.getCustomers();
+					});
 				});
 			};
 			// api calls for orders
 			call.getOrders = function(){
-				return $http.get('/reqauth/api/orders').success(function(data){
-					angular.copy(data, call.orders);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.get('/reqauth/'+data.usertype+'/api/orders').success(function(data){
+							angular.copy(data, call.orders);
+					});
 				});
 			};
 			//moving promise to the controller... might want to do this for the rest...
 			call.createOrder = function(newOrder){
-				return $http.post('/reqauth/api/orders', newOrder)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/orders', newOrder)
+					});
 			};
 			call.updateOrder = function(order){
-				return $http.post('/reqauth/api/orders/update', order).success(function(data){
-					call.getOrders();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/orders/update', order).success(function(data){
+							call.getOrders();
+					});
 				});
 			};
 			call.deleteOrder = function(id){
-				return $http.post('/reqauth/api/orders/delete',{'_id': id}).success(function(data){
-					call.getOrders();
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/orders/delete',{'_id': id}).success(function(data){
+							call.getOrders();
+					});
 				});
 			};
 			call.getOrdersByStatus = function(orderStatus){
-				return $http.get('/reqauth/api/orders/'+orderStatus).success(function(data){
-					angular.copy(data, call.orders);
+				call.getUserInfo()
+					.success(function(data){
+						return $http.get('/reqauth/'+data.usertype+'/api/orders/'+orderStatus).success(function(data){
+							angular.copy(data, call.orders);
+					});
 				});
 			};
 			//send out payment info to server... callback in controller
 			call.runPayment = function(order){
-				return $http.post('/reqauth/api/payments', order)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/payments', order)
+					});
 			};
 			//send order approval to server... callback in controller
 			call.approveOrder = function(order){
-				return $http.post('/reqauth/api/orders/approve', order)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/orders/approve', order)
+					});
 			};
 			call.denyOrder = function(order){
-				return $http.post('/reqauth/api/orders/deny', order)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/orders/deny', order)
+					});
 			};
 			//universal update orderStatus call... need to apply to other places where it should be used.
 			//callback will be put in controllers
 			call.updateOrderStatus=function(order, status){
 				order.orderStatus=status;
-				return $http.post('/reqauth/api/orders/newStatus', order)
+				return $http.post('/reqauth/all/api/orders/newStatus', order)
 			};
 			call.getShippingLabel=function(order){
 				$window.open('https://api.postmaster.io/'+order.sLabel);
 			};
 			call.createUser=function(newUser){
-				return $http.post('/reqauth/api/users', newUser)
+				call.getUserInfo()
+					.success(function(data){
+						return $http.post('/reqauth/'+data.usertype+'/api/users', newUser)
+					});
 			};
 			call.getUserInfo = function(){
-				return $http.get('/reqauth/api/userinfo')
+				return $http.get('/reqauth/all/api/userinfo')
 			};
-					
+
 		return call;
 	}])
 	.factory('toggleServ',[ function(){
@@ -161,7 +224,7 @@ angular.module('MainAppServices',[])
 			"prodName":true,
 			"prodPrice":true
 			};
-		//initialize state of views for approvals	
+		//initialize state of views for approvals
 		toggle.approveToggleState={
 			"_id":true,
 			"pMethod":false,
@@ -194,7 +257,7 @@ angular.module('MainAppServices',[])
 			"tNum":true,
 			"prodName":true,
 			"prodPrice":true
-			};	
+			};
 		toggle.printToggleState={
 			"_id":true,
 			"pMethod":false,
@@ -289,8 +352,8 @@ angular.module('MainAppServices',[])
 			"pMethod": false,
 			"_v":true,
 			"_id": true
-			};	
-		//modify the toggle state, takes in keyword to identify which state to modify	
+			};
+		//modify the toggle state, takes in keyword to identify which state to modify
 		toggle.toggleShow=function(toggleVar, stateMode){
 			var state = function(stateMode){
 				switch(stateMode){
@@ -332,9 +395,8 @@ angular.module('MainAppServices',[])
 						toggle.toggleValues=Object.keys(toggle.customerToggleState);
 						break;
 					};
-			
+
 			};
-			
+
 		return toggle;
 	}])
-
--- routes/reqAuth/reqAuth.js
+++ routes/reqAuth/reqAuth.js
@@ -1,11 +1,12 @@
 var express=require('express')
 var router=express.Router();
 var isLoggedIn = require('../../controllers/loggedin');
-router.use('/', require('./admin/admin'));
-router.use('/', require('./cs/cs'));
-router.use('/', require('./qa/qa'));
+router.use('/all',isLoggedIn.isLoggedIn, require('./all/all'));
+router.use('/cs', require('./cs/cs'));
+router.use('/qa', require('./qa/qa'));
+router.use('/admin', require('./admin/admin'));
 //probably will need later
 //router.use('/', require('./wh/wh'));
 //router.use('/', require('./ret/ret'));
-router.use('/',isLoggedIn.isLoggedIn, require('./all/all'));
+
 module.exports=router;
